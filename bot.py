15110dbd097cf92218439c78111d4d07c43c1dd5
victoryuen <dhuce3@gmail.com>
2023-02-25 16:31:23 -0500

    Update bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+import discord
+import os
+cilent = discord.Cilent()
+
+@cilent.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(cilent))
+
+@cilent.event
+async def on_message(text):
+    if text.author == cilent.user:
+        return
+    
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
